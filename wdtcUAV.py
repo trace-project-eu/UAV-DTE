8472f027a43c709e19bc8a9266e21f30671e0044
savvas-ap <savvas_ap@outlook.com>
2024-09-19 12:34:38 +0300

    Visualization implemented

--- wdtcUAV.py
+++ wdtcUAV.py
@@ -47,26 +47,42 @@ def generateTrajectory(GFZ, locations, flightAltitude, visualize):
             nedPath[i+2][2] = flightAltitude
     else:
         # TODO: add intermediate WPs for geo-fenced zones with A*
-        print("Support of geo-fenced zones to be implemented soon...")
+        # ~~~~~~~~~~~~ temp for visualization development ~~~~~~~~~~~~ #
+        for i in range(0, (len(nedPath)-1)*3, 3):
+            nedPath = np.insert(nedPath, i+1, nedPath[i], 0)
+            nedPath[i+1][2] =  flightAltitude
+            nedPath = np.insert(nedPath, i+2, nedPath[i+2], 0)
+            nedPath[i+2][2] = flightAltitude
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     # visualize path
     if visualize:
         # TODO: Visualize trajectory
         print("Trajectory visualization to be implemented soon...")
-        visualizeTrajectory(GFZ, nedPath)
+        # visualizeTrajectory(GFZ, wgs84path)
 
     # convert ned path to WGS84 coordinates
     wgs84Path = convertor.ned_to_wgs84([nedPath.tolist()])[0]
     return [wgs84Path, nedPath.tolist()]
 
 
-def visualizeTrajectory(GFZ, nedPath):
+def visualizeTrajectory(GFZ, wgs84Path):
+    # convert trajectory to NED
+    convertor = ConvCoords([], [], wgs84Path.tolist()[0])
+    nedPath = convertor.wgs84_to_ned(wgs84Path.tolist())
+
+    # add zero altitude to all GFZ vertices
+    temp = np.array(GFZ[0])
+    GFZ1 = np.zeros((len(GFZ[0]), 3))
+    GFZ1[:,:-1]  = temp
+
+    # convert initial position and destination to NED
+    nedGfzAlt = convertor.wgs84_to_ned(GFZ1.tolist())
+    nedGfz = np.array(nedGfzAlt)[:, 0:2].tolist()
+
     xs = nedPath[:, 0].tolist()
     ys = nedPath[:, 1].tolist()
     zs = nedPath[:, 2].tolist()
-
-    # dummy example of GFZ
-    # poly2D = [(-100, -150), (1500, -250), (800, 500), (-1200, 250), (-1000, -500)]
 
     # Create a figure with subplots
     fig = plt.figure(figsize=(12, 6))
@@ -78,23 +94,34 @@ def visualizeTrajectory(GFZ, nedPath):
     ax1.plot(xs, ys, 'blue', label='Trajectory')
     ax1.set_title('2D view of UAV trajectory')
 
-    # # Plotting the 2D polygon
-    # # poly_x, poly_y = zip(*poly2D)
-    # ax1.fill(poly_x, poly_y, alpha=0.5, color='green', label='2D Polygon')
-    # ax1.legend(loc='lower left')
+    # Plotting the 2D polygon
+    poly_x, poly_y = zip(*nedGfz)
+    ax1.fill(poly_x, poly_y, alpha=0.5, color='green', label='GFZ Polygon')
+    ax1.legend(loc='lower left')
 
     # 3D subplot
     ax2 = fig.add_subplot(122, projection='3d')
-    ax2.axis('equal')
+
+    # Explicitly set axis limits
+    ax2.set_xlim(min(xs), max(xs))
+    ax2.set_ylim(min(ys), max(ys))
+    ax2.set_zlim(min(zs), max(zs))
+
+    # Axis on/off & axis labels
     ax2.axis('off')
+    # ax2.set_xticklabels([])
+    # ax2.set_yticklabels([])
+    # ax2.set_zticklabels([])
+
+    # Plot the 3D trajectory
     ax2.plot3D(xs, ys, zs, 'blue', label='Trajectory')
     ax2.scatter(xs[0], ys[0], zs[0], color='orange', marker='x', label='Initial position')
     ax2.set_title('3D view of UAV trajectory')
 
-    # # Create and plot the 3D polygon
-    # poly3D = [[(x, y, 0) for (x, y) in poly2D]]  # Z-coordinate set to 0
-    # poly3d = Poly3DCollection(poly3D, alpha=0.5, color='green', label='3D Polygon')
-    # ax2.add_collection3d(poly3d)
+    # Create and plot the 3D polygon
+    poly3D = [[(x, y, 0) for (x, y) in nedGfz]]
+    poly3d = Poly3DCollection(poly3D, alpha=0.5, color='green', label='3D Polygon')
+    ax2.add_collection3d(poly3d)
 
     plt.tight_layout()
     plt.show()
