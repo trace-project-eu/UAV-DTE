fc1fc4c8afd8ba543e24b46ffa2fb793269c731f
savvas-ap <savvas_ap@outlook.com>
2024-09-19 17:27:37 +0300

    Visualization of NGZ added

--- visualization.py
+++ visualization.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from shapely.plotting import plot_polygon
+
 from handleGeo.ConvCoords import ConvCoords
 
 
@@ -10,15 +12,6 @@ def visualizeTrajectory(GFZ, wgs84Path):
     # convert trajectory to NED
     convertor = ConvCoords([], [], wgs84Path[0])
     nedPath = convertor.wgs84_to_ned(wgs84Path)
-
-    # add zero altitude to all GFZ vertices
-    temp = np.array(GFZ[0])
-    GFZ1 = np.zeros((len(GFZ[0]), 3))
-    GFZ1[:,:-1]  = temp
-
-    # convert initial position and destination to NED
-    nedGfzAlt = convertor.wgs84_to_ned(GFZ1.tolist())
-    nedGfz = np.array(nedGfzAlt)[:, 0:2].tolist()
 
     xs = nedPath[:, 0].tolist()
     ys = nedPath[:, 1].tolist()
@@ -34,11 +27,6 @@ def visualizeTrajectory(GFZ, wgs84Path):
     ax1.plot(xs, ys, 'blue', label='Trajectory')
     ax1.set_title('2D view of UAV trajectory')
 
-    # Plotting the 2D polygon
-    poly_x, poly_y = zip(*nedGfz)
-    ax1.fill(poly_x, poly_y, alpha=0.35, color='green', label='GFZ Polygon')
-    ax1.legend(loc='lower left')
-
     # 3D subplot
     ax2 = fig.add_subplot(122, projection='3d')
 
@@ -53,10 +41,50 @@ def visualizeTrajectory(GFZ, wgs84Path):
     ax2.scatter(xs[0], ys[0], zs[0], color='orange', marker='x', label='Initial position')
     ax2.set_title('3D view of UAV trajectory')
 
-    # Create and plot the 3D polygon
-    poly3D = [[(x, y, 0) for (x, y) in nedGfz]]
-    poly3d = Poly3DCollection(poly3D, alpha=0.35, color='green', label='3D Polygon')
-    ax2.add_collection3d(poly3d)
+
+    if GFZ:
+        # # add zero altitude to all GFZ vertices
+        # temp = np.array(GFZ[0])
+        # GFZ1 = np.zeros((len(GFZ[0]), 3))
+        # GFZ1[:,:-1]  = temp
+        #
+        # # convert GFZ to NED
+        # nedGfzAlt = convertor.wgs84_to_ned(GFZ1.tolist())
+        # nedGfz = np.array(nedGfzAlt)[:, 0:2].tolist()
+        #
+        # # Plotting the 2D polygon
+        # poly_x, poly_y = zip(*nedGfz)
+        # ax1.fill(poly_x, poly_y, alpha=0.35, color='green', label='GFZ Polygon')
+        # ax1.legend(loc='lower left')
+        #
+        # # Create and plot the 3D polygon
+        # poly3D = [[(x, y, 0) for (x, y) in nedGfz]]
+        # poly3d = Poly3DCollection(poly3D, alpha=0.35, color='green', label='3D Polygon')
+        # ax2.add_collection3d(poly3d)
+        plot_polygon(GFZ[0], convertor, ax1, ax2, 'green', 'Geofence Zone')
+
+        if len(GFZ) > 1:
+            for i in range(1, len(GFZ)):
+                plot_polygon(GFZ[i], convertor, ax1, ax2, 'red', 'No-Go-Zone')
 
     plt.tight_layout()
     plt.show()
+
+def plot_polygon(poly, convertor, ax1, ax2, color, label):
+    temp = np.array(poly)
+    GFZ1 = np.zeros((len(poly), 3))
+    GFZ1[:, :-1] = temp
+
+    # convert GFZ to NED
+    nedGfzAlt = convertor.wgs84_to_ned(GFZ1.tolist())
+    nedGfz = np.array(nedGfzAlt)[:, 0:2].tolist()
+
+    # Plotting the 2D polygon
+    poly_x, poly_y = zip(*nedGfz)
+    ax1.fill(poly_x, poly_y, alpha=0.35, color=color, label=label)
+    ax1.legend(loc='lower left')
+
+    # Create and plot the 3D polygon
+    poly3D = [[(x, y, 0) for (x, y) in nedGfz]]
+    poly3d = Poly3DCollection(poly3D, alpha=0.35, color=color, label=label)
+    ax2.add_collection3d(poly3d)
